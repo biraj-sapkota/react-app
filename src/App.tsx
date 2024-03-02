cb838d7ca8959021e5255c77cecad1aa1901a615
Biraj Sapkota <biraj.intrn@gmail.com>
2024-03-02 20:14:08 +0545

    vesion 2.0

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h5>version 2.0</h5>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
